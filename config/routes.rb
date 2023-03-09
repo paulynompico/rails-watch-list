4d3681b158159e5f0f66de07df79865a9e1b65f1
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-09 16:21:12 +0100

    Added #destroy to bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "lists#index"
 
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
   resources :movies, only: [:index, :show]
 end
