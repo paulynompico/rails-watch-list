974ae942f2ee8009400fb52bc3a8397cf7b69f99
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-09 14:30:04 +0100

    Added list#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "lists#index"
-  
+
+  resources :lists
   resources :movies, only: [:index, :show]
 end
