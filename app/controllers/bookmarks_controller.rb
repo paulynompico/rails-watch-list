3e17289347f99cf503a0ce2cf78bd570d5c4069c
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-09 15:24:44 +0100

    Added bookmark #new #create

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,26 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie, :list)
+  end
 end
