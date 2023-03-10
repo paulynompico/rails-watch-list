e808ea94076ffb2d92f6d4b8c8fac52983d76c4b
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-10 02:11:52 +0100

    Added search bar on navbar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,14 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.search(params[:search])
   end
 
   def show
     @movie = Movie.find(params[:id])
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :search)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,17 @@ class Movie < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+
+  def self.search(search)
+    if search
+      movie = Movie.find_by(title: search)
+        if movie
+          self.where(id: movie)
+        else
+          @movies = Movie.all
+        end
+    else
+      @movies = Movie.all
+    end
+  end
 end
